6a36fb202749366ac67b2bc5659da50d67d3545e
Vikrant Singh <jadounvikrant@gmail.com>
2026-03-16 23:18:09 +0530

    Learn hw to select elements in html using queryselector and by Id, & ClassName

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 // dom = body
 
+//html se document select karna
+//text badalna
+//html badalna
+//css badalna
+// attribute
+// event listeners
+
+
+//1 Slecting elemets --------------->
+
+// let a = document.getElementById("abc")
+
+// let a =document.getElementsByClassName("xyz");
+
+// let a = document.querySelector("h1")
+
+let a = document.querySelectorAll("h1")
+
+console.log(a) /// try console.dir(a)  ---> if not opening
+
